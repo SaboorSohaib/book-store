962bec570c4dbcc2f1a74d8e7a04ec0b8816d046
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-11-24 21:45:59 +0430

    Add some changes

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './book';
 import { fetchBooksFromApi } from '../redux/books/books';
 
 const BookList = () => {
-  const dispatch = useDispatch();
   const books = useSelector((state) => state.books);
 
   return (
