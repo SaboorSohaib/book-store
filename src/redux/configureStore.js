b9ce2d2235c570449a2b2809d55f4d40a59b5910
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-11-26 06:35:53 +0430

    remove commented code.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,6 @@
-// import { combineReducers } from 'redux';
-// import { configureStore } from '@reduxjs/toolkit';
-// import CategoriesReducer from './categories/categories';
-// import books from './books/books';
-
-// const rootReducer = combineReducers({
-//   CategoriesReducer,
-//   books,
-// });
-
-// const store = configureStore({ reducer: rootReducer });
-// export default store;
 import { configureStore } from '@reduxjs/toolkit';
 import books from './books/books';
-// import categories from './Categories/categories';
+import categories from './Categories/categories';
 
 const store = configureStore({
   reducer: {
